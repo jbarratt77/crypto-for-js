fa4d8a9bd7a7029069418181757fa2a394850b06
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2020-06-24 23:06:25 +0100

    :sparkles: other hash functions

--- crypto.js
+++ crypto.js
@@ -9,3 +9,12 @@ console.log(Crypto.SHA256("single string").toString());
 
 const fs = require("fs");
 console.log(Crypto.SHA256(fs.readFileSync('./README.md')).toString());
+
+console.log(Crypto.MD5(124124).toString());
+console.log(Crypto.SHA1("asd").toString());
+console.log(Crypto.SHA256("asdafaf").toString());
+console.log(Crypto.SHA224("asdadad").toString());
+console.log(Crypto.SHA512("qqqq").toString());
+console.log(Crypto.SHA384("asfasfaf").toString());
+console.log(Crypto.SHA3(1).toString());
+console.log(Crypto.RIPEMD160(7).toString());
