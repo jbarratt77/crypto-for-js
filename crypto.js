0781192e33926be5ae1f39f23149077aeb2c76b6
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2020-06-24 23:03:29 +0100

    :sparkles: SHA256 on other data types

--- crypto.js
+++ crypto.js
@@ -2,3 +2,10 @@ const Crypto = require('crypto-js');
 
 console.log(Crypto.SHA256(11));
 console.log(Crypto.SHA256(11).toString());
+
+console.log(Crypto.SHA256({}).toString());
+console.log(Crypto.SHA256([]).toString());
+console.log(Crypto.SHA256("single string").toString());
+
+const fs = require("fs");
+console.log(Crypto.SHA256(fs.readFileSync('./README.md')).toString());
